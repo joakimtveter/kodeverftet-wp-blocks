d2cce9732632c76debf1e0ad6c37d6c56baabab0
Joakim Tveter <joakim.tveter@netpower.no>
2024-04-11 22:31:52 +0200

    Initial Commit

---
+++ kodeverftet-blocks.php
@@ -0,0 +1,14 @@
+/*
+* Plugin Name:       Kodeverftet Blocks
+* Plugin URI:        https://github.com/joakimtveter/kodeverftet-wp-blocks
+* Description:       WordPress Blocks for a section based opinionated experience.
+* Version:           0.0.1
+* Requires at least: 6.0
+* Requires PHP:      8.0
+* Author:            Joakim Tveter
+* Author URI:        https://joakimtveter.no/
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain:       kodeverftet-blocks
+* Domain Path:       /languages
+*/
