57fbf1ec14ac167f7d7182f5f63a068d719974a7
Joakim Tveter <joakim.tveter@netpower.no>
2024-04-11 23:22:04 +0200

    Block Init

--- kodeverftet-blocks.php
+++ kodeverftet-blocks.php
@@ -1,4 +1,6 @@
-/*
+<?php
+/**
+* @wordpress-plugin
 * Plugin Name:       Kodeverftet Blocks
 * Plugin URI:        https://github.com/joakimtveter/kodeverftet-wp-blocks
 * Description:       WordPress Blocks for a section based opinionated experience.
@@ -12,3 +14,20 @@
 * Text Domain:       kodeverftet-blocks
 * Domain Path:       /languages
 */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function kodeverftet_blocks_init() {
+    register_block_type( __DIR__ . '/build' );
+    register_block_type( __DIR__ . '/build/image-text-section' );
+}
+add_action( 'init', 'kodeverftet_blocks_init' );
